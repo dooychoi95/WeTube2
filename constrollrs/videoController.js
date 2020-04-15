8fade43ffb77d54e54cf8aa27ea0448952c8dd08
dooychoi <53390506+dooychoi@users.noreply.github.com>
2020-04-15 15:31:58 +0900

    Add Partials Add Fontawesome

--- constrollrs/videoController.js
+++ constrollrs/videoController.js
@@ -4,4 +4,4 @@ export const vedios = (req, res) => res.render("vedios");
 export const upload = (req, res) => res.render("upload");
 export const videoDetail = (req, res) => res.render("videoDetail");
 export const editVideo = (req, res) => res.render("editVideo");
-export const deleteVideo = (req, res) => res.render("deleteVideo");
+export const deleteVideo = (req, res) => re 
